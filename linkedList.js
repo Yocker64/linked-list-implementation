0294abb92f5e4676a678c4cdfe1f7f57f324c405
Sandoval Mario <mdyoc74@gmail.com>
2025-06-17 20:04:58 +0900

    Add methods: pop, contains, find and toString

--- linkedList.js
+++ linkedList.js
@@ -4,7 +4,7 @@ class LinkedList {
 
   constructor(head) {
     this.head= new Node(head);
-    this.tail = head;
+    this.tail = this.head;
     this.size = 1;
     
     
@@ -21,6 +21,55 @@ append(value) {
     newHead.nextNode = this.tail;
     this.head = newHead;
     this.size ++;
+  }
+
+  at(index){
+    let returnNode = this.head
+    for (let i = 0; i < index; i++) {
+      returnNode = returnNode.nextNode
+    }
+    return returnNode
+  }
+
+  pop(){
+    this.tail = this.at(this.size-1)
+    this.tail.nextNode = undefined
+    this.size --;
+  }
+
+  toString(){
+    let currentNode=  this.head;
+    for (let i = 0; i < this.size-1; i++) {
+      console.log(`(${currentNode.data})->`)
+      currentNode = currentNode.nextNode
+    }
+      console.log(`(${currentNode.data})`)
+
+  }
+  contains(value){
+    let currentNode = this.head
+    for (let i = 0; i < this.size; i++) {
+      if (currentNode.data == value) {
+        return true
+      }
+      currentNode = currentNode.nextNode
+      
+    }
+    return false
+  }
+
+  find(value){
+    let currentNode = this.head;
+    let index= 0;
+    for (let i = 0; i < this.size; i++) {
+      if (currentNode.data == value) {
+        return index
+      }
+      currentNode = currentNode.nextNode;
+      index ++;
+      
+    }
+    return undefined
   }
 
   }
